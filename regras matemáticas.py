672e2dab221326124191d1e45a016b92cd74c096
Israel Cordeiro de Andrade <raelcordeiro2@gmai.com>
2024-07-15 19:14:49 -0300

    Adicionar regras matemáticas

---
+++ regras matemáticas.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+n3 = int(input('digite mais um numero: '))
+n4 = int(input('digite mais um numero: '))
+s = n1 + n2 / n3 * n4
+print('O valor de {} + {} / {} x {} é igual: {}'.format( n1, n2, n3, n4, s))
